1ba28566fefc0941ea8508d0e9597f06c2898af0
Jewoo Lee <jwcairo0502@gmail.com>
2025-06-28 11:43:37 -0400

    wip-primary button

---
+++ src/components/Button.jsx
@@ -0,0 +1,72 @@
+/**
+ * @copyright 2025 Jewoo Lee
+ * @license <Apache-2 className="0"></Apache-2>
+ */
+
+
+/**
+ * Node modules
+ */
+import PropTypes from 'prop-types';
+
+/**
+ * Primary Button
+ */
+
+const ButtonPrimary = ({
+  href,
+  target = '_self',
+  label,
+  icon,
+  classes
+}) => {
+  if (href) {
+    return (
+      <a
+        href={href}
+        target={target}
+        className={"btn btn-primary " + classes}
+      >
+        {label}
+
+        {icon ?
+          <span
+            className='material-symbols-rounded'
+            aria-hidden="true"
+          >
+            {icon}
+          </span>
+          : undefined
+        }
+      </a>
+    )
+  } else {
+    return (
+      <button className={"btn btn-primary " + classes}>
+        {label}
+
+        {icon ?
+          <span
+            className='material-symbols-rounded'
+            aria-hidden="true"
+          >
+            {icon}
+          </span>
+          : undefined
+        }
+      </button>
+    )
+  }
+}
+
+ButtonPrimary.propTypes = {
+  label: PropTypes.string.isRequired,
+  href: PropTypes.string,
+  target: PropTypes.string,
+  icon: PropTypes.string,
+  classes: PropTypes.string
+}
+
+export {
+  ButtonPrimary
+}
