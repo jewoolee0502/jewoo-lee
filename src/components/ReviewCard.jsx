a7d568ce08d99a391c20603375b72d5852b68819
Jewoo Lee <jwcairo0502@gmail.com>
2025-07-02 18:22:14 -0400

    wip-reviewCard component

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -21,13 +21,13 @@ const ReviewCard = ({
   company
 }) => {
   return (
-    <div className="">
+    <div className="bg-zinc-800 p-5 rounded-xl min-w-[320px] flex flex-col lg:min-w-[420px]">
 
-      <div className="">
+      <div className="flex items-center gap-1 mb-3">
         {ratings.map(({ icon, style }, key) => (
           <span
             key={key}
-            className="material-symbols-rounded"
+            className="material-symbols-rounded text-yellow-300 text-[18px]"
             style={style}
           >
             {icon}
@@ -35,12 +35,12 @@ const ReviewCard = ({
         ))}
       </div>
 
-      <p className="">
+      <p className="text-zinc-400 mb-8">
         {content}
       </p>
 
-      <div className="">
-        <figure className="">
+      <div className="flex items-center gap-2 mt-auto">
+        <figure className="img-box rounded-lg">
           <img
             src={imgSrc}  
             alt={name}
@@ -54,7 +54,7 @@ const ReviewCard = ({
         <div>
           <p>{name}</p>
 
-          <p className="">
+          <p className="text-xs text-zinc-400 tracking-wider">
             {company}
           </p>
         </div>
