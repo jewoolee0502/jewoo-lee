efda3b635d05657b6a6a67b874a2f760a1e3fbe4
Jewoo Lee <jwcairo0502@gmail.com>
2025-07-14 16:46:34 -0400

    wip-footer component created

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import Skill from "./components/Skill";
 import Work from "./components/Work";
 import Review from "./components/Review";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 
 const App = () => {
