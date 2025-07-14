ffb1d28e93ffe76c011dca9a6a94d7dfec199e37
Jewoo Lee <jwcairo0502@gmail.com>
2025-07-14 16:48:15 -0400

    wip

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,13 +3,56 @@
  * @license Apache-2.0
  */
 
+const sitemap = [
+  {
+    label: 'Home',
+    href: '#home'
+  },
+  {
+    label: 'About',
+    href: '#about'
+  },
+  {
+    label: 'Work',
+    href: '#work'
+  },
+  {
+    label: 'Reviews',
+    href: '#reviews'
+  },
+  {
+    label: 'Contact me',
+    href: '#contact'
+  }
+];
+
+const socials = [
+  {
+    label: 'GitHub',
+    href: 'https://www.github.com/codewithsadee-org'
+  },
+  {
+    label: 'LinkedIn',
+    href: 'https://www.linkedin.com/in/codewithsadee'
+  },
+  {
+    label: 'Twitter X',
+    href: 'https://x.com/codewithsadee_'
+  },
+  {
+    label: 'Instagram',
+    href: 'https://www.instagram.com/codewithsadee'
+  },
+  {
+    label: 'CodePen',
+    href: 'https://codepen.io/codewithsadee'
+  }
+];
 
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <div>Footer</div>
   )
 }
 
