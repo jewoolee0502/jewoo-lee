a863cc6976c1053fef0088c3b6cc1cde77a1677e
Jewoo Lee <jwcairo0502@gmail.com>
2025-07-16 11:10:25 -0400

    project updates

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -10,40 +10,22 @@ import ProjectCard from "./ProjectCard";
 
 const works = [
   {
-    imgSrc: '/images/project-1.jpg',
-    title: 'Full stack music app',
-    tags: ['API', 'MVC', 'Development'],
-    projectLink: 'https://musify-5al0.onrender.com/'
+    imgSrc: '/images/MenuLens.png',
+    title: 'Menu Lens',
+    tags: ['API', 'OCR', 'AI Prompting', 'Prompt Engineering', 'Python'],
+    projectLink: 'https://devpost.com/software/menulens'
   },
   {
-    imgSrc: '/images/project-2.jpg',
-    title: 'Free stock photo app',
-    tags: ['API', 'SPA'],
-    projectLink: 'https://pixstock-official.vercel.app/'
+    imgSrc: '/images/MarletNests.png',
+    title: 'Marlet Nests',
+    tags: ['API', 'Python', 'Realtime Database'],
+    projectLink: 'https://devpost.com/software/martlet-s-nest'
   },
   {
-    imgSrc: '/images/project-3.jpg',
-    title: 'Recipe app',
-    tags: ['Development', 'API'],
-    projectLink: ''
-  },
-  {
-    imgSrc: '/images/project-4.jpg',
-    title: 'Real state website',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee-org/wealthome'
-  },
-  {
-    imgSrc: '/images/project-5.jpg',
-    title: 'eCommerce website',
-    tags: ['eCommerce', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/anon-ecommerce-website'
-  },
-  {
-    imgSrc: '/images/project-6.jpg',
-    title: 'vCard Personal portfolio',
-    tags: ['Web-design', 'Development'],
-    projectLink: 'https://github.com/codewithsadee/vcard-personal-portfolio'
+    imgSrc: '/images/adStar.png',
+    title: 'adStar',
+    tags: ['Machine Learning', 'API', 'Data Analysis'],
+    projectLink: 'https://devpost.com/software/adstar'
   },
 ];
 
