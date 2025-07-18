3b2894e422480a2b670b4c1f0e1371b4497362cd
Jewoo Lee <jwcairo0502@gmail.com>
2025-07-18 11:38:02 -0400

    wip

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -46,26 +46,41 @@ const Experience = () => {
   return (
       <section
         id="experience"
-        className="section"
+        className="section py-16 bg-neutral-900 text-white"
       >
-        <div className="container">
+        <div className="container mx-auto px-4 max-w-5xl">
   
           <h2 className="headline-2 mb-8 reveal-up">
             My Career Highlights
           </h2>
   
-          {/* <div className="grid gap-x-4 gap-y-5 grid-cols-[repeat(auto-fill,_minmax(280px,_1fr))]">
-            {experiences.map(({ title, company, date, description }, key) => (
-              <ProjectCard 
-                key={key}
-                title={title}
-                company={company}
-                date={date}
-                description={description}
-                classes="reveal-up"
-              />
-            ))}
-          </div> */}
+          <div className="relative">
+            <div className="absolute left-1/2 top-0 transform -translate-x-1/2 h-full w-1bg-gray-700"></div>
+
+            <div className="flex flex-col space-y-16">
+              {experiences.map((exp, index) => (
+                <div
+                  key={index}
+                  className="relative flex flex-col lg:flex-row lg:items-start"
+                >
+                  {/* Circles */}
+                  <div className="absolute left-1/2 transform -translate-x-1/2 bg-white border-4 border-gray-800 w-6 h-6 rounded-full z-10"></div>
+
+                  {/* Left Content */}
+                  <div className="lg:w-1/2 lg:pr-12 text-right">
+                    <h3 className="text-lg font-semibold">{exp.title}</h3>
+                    <p className="text-gray-400 italic">{exp.company}</p>
+                    <p className="text-sm text-gray-500 mt-1">{exp.date}</p>
+                  </div>
+
+                  {/* Right Content */}
+                  <div className="lg:w-1/2 lg:pl-12 mt-6 lg:mt-0">
+                    <p className="text-gray-300">{exp.description}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
   
         </div>
       </section>
